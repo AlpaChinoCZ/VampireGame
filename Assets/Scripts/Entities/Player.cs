c4a388695662452f7e4e392194c1e25b8b7bec31
AlpaChinoCZ <michal.dvorak99@seznam.cz>
2024-08-05 19:33:14 +0200

    player is singleton

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -8,6 +8,33 @@ namespace VG
 {
     public class Player : Actor
     {
-        
+        private static Player instance;
+
+        public static Player Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<Player>();
+                    if (instance == null)
+                    {
+                        instance = new GameObject().AddComponent<Player>();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public UnityEvent onShoot;
+
+        public override void Awake()
+        {
+            base.Awake();
+    
+            if (instance != null) Destroy(this);
+            DontDestroyOnLoad(this);
+        }
+
     }
 }
