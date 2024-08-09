dbad592190c514fbf64ff47c24cb752d49900d44
AlpaChinoCZ <michal.dvorak99@seznam.cz>
2024-08-10 00:48:07 +0200

    Player Singleton removed. components added

--- Assets/Scripts/Entities/PlayerController.cs
+++ Assets/Scripts/Entities/PlayerController.cs
@@ -10,32 +10,22 @@ namespace VG
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private Player player;
         [SerializeField] private LayerMask clickableLayers;
-        
-        public static PlayerController Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = FindObjectOfType<PlayerController>();
-                    if (instance == null)
-                    {
-                        instance = new GameObject().AddComponent<PlayerController>();
-                    }
-                }
-                return instance;
-            }
-        }
-
-        public Vector2 moveInput = new Vector2(0,0);
+        [SerializeField] private LayerMask mouseMovementLayers;
             
-        private static PlayerController instance;
         private PlayerInputAction playerInputAction;
         private Camera playerCamera;
         private Dictionary<ActionMap, string> actionMaps;
-
+        private Player player;
+        private Rigidbody playerBody;
+        private Vector2 mouseLook;
+        
+        public void Init(Player player, Camera camera)
+        {
+            this.player = player;
+            playerCamera = camera;
+        }
+        
         public void SwitchActionMap(ActionMap newActionMap)
         {
             foreach (var map in playerInputAction.asset.actionMaps)
@@ -48,63 +38,75 @@ namespace VG
         
         private void Awake()
         {
-            if (instance != null) Destroy(this);
             DontDestroyOnLoad(this);
-            
             actionMaps = new Dictionary<ActionMap, string>
             {
                 { ActionMap.Player, "Player" },
                 { ActionMap.UI, "UI" }
             };
-            playerCamera = Camera.main;
 
-            playerInputAction = new PlayerInputAction();
             
-            Assert.IsNotNull(player, $"{gameObject} player is null.");
-            Assert.IsNotNull(playerCamera, $"{gameObject} main Player Camera is null.");
+            
+            playerInputAction = new PlayerInputAction();
         }
         
+        private void Update()
+        {
+            if (RaycastCameraToMouse(out var hitResult, mouseMovementLayers))
+            {
+                player.MovementController.SetLookTarget(hitResult.point);
+            }
+        }
+
         private void OnEnable()
         {
             playerInputAction.Enable();
             playerInputAction.Player.Move.performed += SetMoveInput;
             playerInputAction.Player.Move.canceled += SetMoveInput;
-            playerInputAction.Player.Shoot.performed += Shoot;
+            playerInputAction.Player.Jump.started += Jump;
+            playerInputAction.Player.MouseLook.performed += MouseLook;
+            playerInputAction.Player.Shoot.started += Shoot;
         }
         
         private void OnDisable()
         {
             playerInputAction.Player.Move.performed -= SetMoveInput;
             playerInputAction.Player.Move.canceled -= SetMoveInput;
+            playerInputAction.Player.Jump.started -= Jump;
+            playerInputAction.Player.MouseLook.performed -= MouseLook;
+            playerInputAction.Player.Shoot.started -= Shoot;
             playerInputAction.Disable();
         }
-
-        private void FixedUpdate()
-        {
-            UpdateVelocity();
-        }
-
+        
         private void SetMoveInput(InputAction.CallbackContext context)
         {
-            moveInput = context.ReadValue<Vector2>();
-        }
-
-        private void Shoot(InputAction.CallbackContext context)
-        {
-            var initPos = player.transform.position;
-            var mouseRay = playerCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-            if (Physics.Raycast(mouseRay, out RaycastHit hit, 500f, clickableLayers))
-            {
-                player.LaunchProjectile.Launch(player.transform.position, hit.transform.position);
-            }
-        }
-
-        private void UpdateVelocity()
-        {
-            player.Body.velocity = new Vector3(moveInput.x * player.MovementSpeed, player.Body.velocity.y, moveInput.y * player.MovementSpeed);
+            player.MovementController.SetVelocity(context.ReadValue<Vector2>());
         }
         
+        private void Jump(InputAction.CallbackContext context)
+        {
+            player.MovementController.Jump();
+        }
+        
+        private void MouseLook(InputAction.CallbackContext context)
+        {
+            mouseLook = context.ReadValue<Vector2>();
+        }
+        
+        private void Shoot(InputAction.CallbackContext context)
+        {
+            if(RaycastCameraToMouse(out RaycastHit hitResult, clickableLayers))
+            {
+                player.ShootingComponent.Launch(player.transform.position, hitResult.transform.position);
+            }
+        }
+        
+        private bool RaycastCameraToMouse(out RaycastHit hitResult, LayerMask layers)
+        {
+            var mouseRay = playerCamera.ScreenPointToRay(mouseLook);
+            return Physics.Raycast(mouseRay, out hitResult, Mathf.Infinity, layers);
+        }
+
         public enum ActionMap
         {
             UI,
