c8cc291dffcb882872e689c3e8dd9c5e665c0b81
AlpaChinoCZ <michal.dvorak99@seznam.cz>
2024-08-10 00:48:31 +0200

    Revert "Player Singleton removed. components added"
    This reverts commit dbad592190c514fbf64ff47c24cb752d49900d44.

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -2,31 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.Events;
 
 namespace VG
 {
-    [RequireComponent(typeof(Rigidbody))]
     public class Player : Actor
     {
-        [SerializeField] private Shooting shootingComponent;
-        [SerializeField] private MovementController movementController;
-        
-        public Shooting ShootingComponent=> shootingComponent;
-        public MovementController MovementController => movementController;
+        private static Player instance;
 
-        private Rigidbody body;
+        public static Player Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<Player>();
+                    if (instance == null)
+                    {
+                        instance = new GameObject().AddComponent<Player>();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public UnityEvent onShoot;
 
         public override void Awake()
         {
             base.Awake();
-
-            body = GetComponent<Rigidbody>();
-            movementController = GetComponent<MovementController>();
-            
-            Assert.IsNotNull(MovementController, $"{gameObject} movement component is null");
-            Assert.IsNotNull(shootingComponent, $"{gameObject} launch projectile is null");
+    
+            if (instance != null) Destroy(this);
+            DontDestroyOnLoad(this);
         }
+
     }
 }
