39ce321d58a15766c3d638097d833bd38b5f4cbf
AlpaChinoCZ <michal.dvorak99@seznam.cz>
2024-08-11 14:45:04 +0200

    enemy info color removed, renamed properties

--- Assets/Scripts/Info/EnemyInfo.cs
+++ Assets/Scripts/Info/EnemyInfo.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 namespace VG
 {
-    [CreateAssetMenu(fileName = "ActorInfo", menuName = "VG/Info/ActorInfo")]
+    [CreateAssetMenu(fileName = "EnemyInfo", menuName = "VG/Info/EnemyInfo")]
     public class EnemyInfo : ScriptableObject
     {
-        [SerializeField] private string actorName = "none";
-        [SerializeField] private Color color = Color.blue;
+        [SerializeField] private string enemyName = "none";
         [SerializeField] private float speed = 20f;
         [SerializeField] private float fireRate = 1f;
         [Tooltip("Distance from which the enemy starts firing")]
         [SerializeField] private float startFireDistance = 10f;
 
-        public string Name => actorName;
-        public Color Color => color;
+        public string Name => enemyName;
         public float Speed => speed;
         public float FireRate => fireRate;
         public float StartFireDistance => startFireDistance;
