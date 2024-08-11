2d7eac704b4016f22d01f93cff4098e17dfc9ee3
AlpaChinoCZ <michal.dvorak99@seznam.cz>
2024-08-11 11:24:45 +0200

    base enemy class

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -2,22 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
 using UnityEngine.Events;
 
 namespace VG
 {
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class Enemy : Actor
     {
         [SerializeField] private EnemyInfo enemyInfo;
+        [Tooltip("Target towards which the Enemy will move")]
+        [SerializeField] private Transform target;
+        [SerializeField] private BasicFire fireComponent;
+        
+        private Rigidbody body;
+        private NavMeshAgent navMeshAgent;
 
         public EnemyInfo Info => enemyInfo;
-
-        public UnityEvent onShoot;
 
         public override void Awake()
         {
             base.Awake();
-    
+
+            body = GetComponent<Rigidbody>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+
+            if (target == null)
+            {
+                var player = FindObjectOfType(typeof(Player)) as GameObject;
+                target = player != null ? player.transform : null;
+            }
+            
+            Assert.IsNotNull(enemyInfo, $"{gameObject} have to have Enemy Info");
+            Assert.IsNotNull(target, $"{gameObject} have to have target");
+        }
+
+        public void Start()
+        {
+            navMeshAgent.SetDestination(target.transform.position);
         }
     }
 }
