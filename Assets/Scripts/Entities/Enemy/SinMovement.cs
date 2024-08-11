7d2f5d0fe201820aa8ae8044014ae6b224727960
AlpaChinoCZ <michal.dvorak99@seznam.cz>
2024-08-11 12:23:23 +0200

    sinus movement implementation

--- Assets/Scripts/Entities/Enemy/SinMovement.cs
+++ Assets/Scripts/Entities/Enemy/SinMovement.cs
@@ -1,7 +1,29 @@
+using System;
+using UnityEngine;
+
 namespace VG
 {
-    public class SinMovement
+    public class SinMovement : MonoBehaviour
     {
+        [Tooltip("Height of the wave")]
+        [SerializeField] private float waveHeight = 2.0f;
+        [Tooltip("Frequency of the wave")]
+        [SerializeField] private float waveFrequency = 1.0f;
+
+        private Vector3 initialPosition;
         
+        private void Start()
+        {
+            initialPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            var yOffset = Mathf.Sin(Time.time * waveFrequency) * waveHeight;
+
+            var newPosition = transform.position;
+            newPosition.y = initialPosition.y + yOffset;
+            transform.position = newPosition;
+        }
     }
 }
